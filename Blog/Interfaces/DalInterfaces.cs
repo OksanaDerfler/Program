dde9f3cd0f30f029e9a61c54c96264e946879e08
Kir <kirxard@gmail.com>
2016-06-13 02:35:24 +0300

    new

---
+++ Blog/Interfaces/DalInterfaces.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+
+namespace Interfaces
+{
+    public interface IRecord
+    {
+        IEnumerable<Record> GetAll();
+        Record Get(int id);
+        bool Create(Record record);
+        bool Update(int id);
+        bool Delete(int id);
+    }
+}
