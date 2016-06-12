f957d654b8a1d10d553a44ef27377c2ba3171b87
Kir <kirxard@gmail.com>
2016-06-13 02:17:53 +0300

    new bll and dall and more

---
+++ Blog/Entities/Record.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Record
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string text { get; set; }
+        public string dateStart { get; set;}
+        public string tag { get; set; }
+        public int like { get; set; }
+        public string nick { get; set; }
+        public byte[] picture { get; set; }
+    }
+
+}
