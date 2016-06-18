1a7a4cb1e4309756055c684115bbf5f50bfec147
Kir <kirxard@gmail.com>
2016-06-19 01:44:20 +0300

    new

---
+++ Blog/BLL/Bll.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Bll
+    {
+        public List<string> GetDistinctTags()
+        {
+            var db = new DAL.Recordd().GetAll();
+            return db.Select(p => p.Tag).Distinct().ToList();
+        }
+    }
+}
